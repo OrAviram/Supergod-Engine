2a0494be5ebce7880902ec4cf105e0ca33222854
Or Aviram <or2.aviram@gmail.com>
2017-09-03 22:31:01 +0300

    Added some useful typedefs. I still need to make a good folder structure, though.

--- SupergodEngineTesting/MathTests.cpp
+++ SupergodEngineTesting/MathTests.cpp
@@ -10,7 +10,7 @@ namespace SupergodEngineTesting
 			Assert::IsTrue(SMath::CloseEnough(1, 2, 1));
 			Assert::IsTrue(SMath::CloseEnough(1, 1.0000001f, 0.000001f));
 			Assert::IsTrue(SMath::CloseEnough(1, 1.0000001f));
-
+			
 			Assert::IsFalse(SMath::CloseEnough(1, 2, .5f));
 			Assert::IsFalse(SMath::CloseEnough(1, 1.0000001f, 0.000000001f));
 		}
--- SupergodsSandbox/Main.cpp
+++ SupergodsSandbox/Main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include "../SupergodEngine/Math/SMath.h"
 
 void main()
 {
-	std::cout << "Things" << std::endl;
+	byte b = -3;
+	std::cout << SupergodEngine::Math::SMath::CloseEnough(1, 2, 1) << std::endl;
+	std::cout << SupergodEngine::Math::SMath::CloseEnough(1, 2, 0.999f) << std::endl;
 	std::cin.get();
 }
