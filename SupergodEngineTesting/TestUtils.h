1725a5a3c59485abd83c1dd3ed8d8cfeebc03368
Or Aviram <or2.aviram@gmail.com>
2017-09-03 16:19:41 +0300

    Setup folder structure.

---
+++ SupergodEngineTesting/TestUtils.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#include <CppUnitTest.h>
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
---
+++ SupergodsSandbox/Main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+void main()
+{
+	std::cout << "Things" << std::endl;
+	std::cin.get();
+}
