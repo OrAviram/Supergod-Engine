98ec14b4308d0f26da06d3d5610d349f1ba505dc
Or Aviram <or2.aviram@gmail.com>
2017-09-19 23:16:58 +0300

    Deleted a useless comment.

--- SupergodEngine/Math/Vectors/IVector.h
+++ SupergodEngine/Math/Vectors/IVector.h
@@ -16,8 +16,6 @@ namespace std
 
 namespace SupergodEngine { namespace Math
 {
-	// TODO: Add the methods for finding the angle when the Angle struct is added.
-
 	/// <summary>
 	/// Interface vector structs can implement.<para/>
 	/// T is the type of the vector.
